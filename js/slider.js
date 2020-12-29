0b6066df00f7149b8af1700761514c888465b9c1
Marco Ramirez <mramirezp@est.utn.ac.cr>
2020-12-29 03:26:37 -0600

    Add slider and about section, and footer

---
+++ js/slider.js
@@ -0,0 +1,7 @@
+new fullpage('#fullpage',{
+    autoScrolling: false,
+    navigation: true,
+    anchors:['home','menu','about','services','work','contact'],
+    navigationTooltips: ['Home','Menu','About','Services','Work','Contact'],
+    showActiveTooltip: true
+})
